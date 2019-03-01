363851184ca213ea39884561dd638ec979756ae0
MrxColin <colin.treal@gmail.com>
2019-03-01 15:20:49 +0100

    Modif du modele

--- model.py
+++ model.py
@@ -2,11 +2,14 @@ from keras.models import Sequential
 from keras.layers import Dense
 #create model
 model = Sequential()
+train_dataset = np.load('X.npy') 
+#one-hot encode target column
 
+#vcheck that target column has been converted
+n_images= len(train_dataset)
 #add model layers
-model.add(Dense(200, activation='relu', input_shape=(n_cols,)))
+model.add(Dense(200, activation='relu', input_shape=(n_images,)))
 model.add(Dense(200, activation='relu'))
 model.add(Dense(200, activation='relu'))
 model.add(Dense(200, activation='relu'))
-
-model.add(Dense(200))
+model.add(Dense(1, activation='softmax'))
