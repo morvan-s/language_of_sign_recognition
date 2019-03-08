1ac8217ead4be9a433820ca3ba0bdc3a07a40c81
MrxColin <colin.treal@gmail.com>
2019-03-08 14:42:50 +0100

    ajout model simple

--- model.py
+++ model.py
@@ -16,15 +16,6 @@ y_test= np.load('Y.npy')
 n_cols = X_train.shape[1]
 
 
-
-# 5. Preprocess input data
-X_train = X_train.reshape(X_train.shape[0], 1, 64, 64)
-X_test = X_test.reshape(X_test.shape[0], 1, 64, 64)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-
 # 6. Preprocess class labels
 Y_train = np_utils.to_categorical(y_train, 9)
 Y_test = np_utils.to_categorical(y_test, 9)
@@ -34,10 +25,14 @@ print(X_train.shape)
 model = Sequential()
 
 #add model layers
-model_mc.add(Dense(200, activation='relu', input_shape=(n_cols,)))
-model_mc.add(Dense(200, activation='relu'))
-model_mc.add(Dense(200, activation='relu'))
-model_mc.add(Dense(1))
+
+
+model.add(Dense(250, activation='relu', input_shape=(n_cols,)))
+model.add(Dense(250, activation='relu'))
+model.add(Dense(250, activation='relu'))
+model.add(Dense(250, activation='relu'))
+model.add(Dense(250, activation='relu'))
+model.add(Dense(2, activation='softmax'))
 
 # 8. Compile model
 model.compile(loss='categorical_crossentropy',
