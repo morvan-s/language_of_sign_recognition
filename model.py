77e1b963f947cfbc27cdb9e5f7d791c7bbd54b2e
morvan-s <stiven.morvan@gmail.com>
2019-03-12 15:15:34 +0100

    ajout model 3

--- model.py
+++ model.py
@@ -23,14 +23,16 @@ image_gen  = ImageDataGenerator(
 unique, count= np.unique(Y_train, return_counts=True)
 
 model = Sequential()
-model.add(Convolution2D(10, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
+model.add(Convolution2D(16, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
-model.add(Convolution2D(20, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
+model.add(Convolution2D(32, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
-model.add(Convolution2D(30, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
+model.add(Convolution2D(64, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
-model.add(Convolution2D(40, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
+model.add(Convolution2D(128, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+model.add(Dropout(0.25))
+
 model.add(Flatten())
 model.add(Dropout(0.5))
 model.add(Dense(128, activation='relu'))
@@ -50,7 +52,7 @@ batch_size = 128
 #           epochs=30,
 #           verbose=1)
 # 9. Fit model on training data
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=40, verbose=1)
+model.fit(X_train, Y_train, batch_size=32, nb_epoch=15, verbose=1)
 
 score = model.evaluate(X_eval, Y_eval, verbose=0)
 print(score)
