761d853430cb05d9fc8338ce082c3454a2ca3d98
MrxColin <colin.treal@gmail.com>
2019-03-18 09:24:30 +0100

    ajout des lignes de data augmentation, il faut regarder ce qui ne va pas

--- model.py
+++ model.py
@@ -4,7 +4,6 @@ from keras.utils import np_utils, to_categorical
 from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
 
-import tensorflowjs as tfjs
 import numpy as np
 import dataset
 import cv2
@@ -14,22 +13,24 @@ model = Sequential()
 
 X_train, Y_train, X_eval, Y_eval  = dataset.get_dataset()
 
-image_gen  = ImageDataGenerator(
-        rotation_range=30,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        rescale=1.0/255,
-        zoom_range=0.2)
+image_gen = ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=False)
+
 unique, count= np.unique(Y_train, return_counts=True)
 
 model = Sequential()
-model.add(Convolution2D(16, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
-model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
-model.add(Convolution2D(32, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
+model.add(Convolution2D(32, (3, 3), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
 model.add(Convolution2D(64, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
 model.add(Convolution2D(128, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+model.add(Convolution2D(256, (2, 2), activation='relu', input_shape=(64,64,1), data_format='channels_last'))
 model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
 model.add(Dropout(0.25))
 
@@ -42,18 +43,21 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-#image_gen.fit(X_train)
+image_gen.fit(X_train)
+
+
 
 
 batch_size = 128
 
-# model.fit_generator(image_gen.flow(X_train, Y_train, batch_size=batch_size),
-#           steps_per_epoch=  X_train.shape[0]//batch_size,
-#           epochs=30,
-#           verbose=1)
-# 9. Fit model on training data
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=15, verbose=1)
+#model.fit_generator(image_gen.flow(X_train, Y_train, batch_size=128),
+                    #steps_per_epoch=len(X_train) / 128, epochs=30, validation_data=(X_eval,Y_eval))
 
-score = model.evaluate(X_eval, Y_eval, verbose=0)
-print(score)
-tfjs.converters.save_keras_model(model, 'demo/model4')
+# 9. Fit model on training data
+model.fit(X_train, Y_train, batch_size=32, nb_epoch=15, verbose=1, validation_data=(X_eval,Y_eval))
+
+score, acc = model.evaluate(X_eval, Y_eval, batch_size=128)
+
+print('Test score:', score)
+print('Test accuracy:', acc)
+#tfjs.converters.save_keras_model(model, 'demo/model4')
